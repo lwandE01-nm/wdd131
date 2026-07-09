2c79786a8a81612acbfdf37283417db1d1f9893c
lwandE01-nm <lwandE01@icloud.com>
2026-07-09 20:07:34 +0200

    Add navigation toggle and footer date display

---
+++ scripts/temples.js
@@ -0,0 +1,17 @@
+const menuButton = document.querySelector("#menu");
+const navigation = document.querySelector(".navigation");
+
+menuButton.addEventListener("click", () => {
+    navigation.classList.toggle("open");
+
+    if (navigation.classList.contains("open")) {
+        menuButton.textContent = "✖";
+    } else {
+        menuButton.textContent = "☰";
+    }
+});
+
+document.querySelector("#year").textContent = new Date().getFullYear();
+
+document.querySelector("#lastModified").textContent =
+`Last Modified: ${document.lastModified}`;
