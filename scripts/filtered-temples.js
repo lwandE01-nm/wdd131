810b9d6baa8f334adb9e8f1de7428de18918fa4d
lwandE01-nm <lwandE01@icloud.com>
2026-07-23 10:51:54 +0200

    Update filtered-temples.js

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -1,88 +1,85 @@
 const temples = [
-    {
-        templeName: "Salt Lake Temple",
-        location: "Salt Lake City, Utah, USA",
-        dedicated: "1893, April, 6",
-        area: 253000,
-        imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/salt-lake-temple/salt-lake-temple-37763-main.jpg"
-    },
-    {
-        templeName: "Manti Utah Temple",
-        location: "Manti, Utah, USA",
-        dedicated: "1888, May, 21",
-        area: 74792,
-        imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/manti-utah-temple/manti-utah-temple-37775-main.jpg"
-    },
-    {
-        templeName: "Laie Hawaii Temple",
-        location: "Laie, Hawaii, USA",
-        dedicated: "1919, November, 27",
-        area: 42100,
-        imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/laie-hawaii-temple/laie-hawaii-temple-7750-main.jpg"
-    },
-    {
-        templeName: "Rome Italy Temple",
-        location: "Rome, Italy",
-        dedicated: "2019, March, 10",
-        area: 41010,
-        imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/rome-italy-temple/rome-italy-temple-12203-main.jpg"
-    },
-    {
-        templeName: "Johannesburg South Africa Temple",
-        location: "Johannesburg, South Africa",
-        dedicated: "1985, August, 24",
-        area: 19184,
-        imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/johannesburg-south-africa-temple/johannesburg-south-africa-temple-37808-main.jpg"
-    },
-    {
-        templeName: "Durban South Africa Temple",
-        location: "Durban, South Africa",
-        dedicated: "2020, February, 16",
-        area: 19860,
-        imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/durban-south-africa-temple/durban-south-africa-temple-3919-main.jpg"
-    },
-    {
-        templeName: "Mexico City Temple",
-        location: "Mexico City, Mexico",
-        dedicated: "1983, December, 2",
-        area: 116642,
-        imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/mexico-city-mexico-temple/mexico-city-mexico-temple-4060-main.jpg"
-    },
-    {
-        templeName: "Paris France Temple",
-        location: "Paris, France",
-        dedicated: "2017, May, 21",
-        area: 44000,
-        imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/paris-france-temple/paris-france-temple-9373-main.jpg"
-    },
-    {
-        templeName: "Nauvoo Illinois Temple",
-        location: "Nauvoo, Illinois, USA",
-        dedicated: "2002, June, 27",
-        area: 58000,
-        imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/nauvoo-illinois-temple/nauvoo-illinois-temple-37910-main.jpg"
-    },
-    {
-        templeName: "Lima Peru Temple",
-        location: "Lima, Peru",
-        dedicated: "1986, January, 10",
-        area: 9600,
-        imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/lima-peru-temple/lima-peru-temple-37793-main.jpg"
-    },
-    {
-        templeName: "Hamilton New Zealand Temple",
-        location: "Hamilton, New Zealand",
-        dedicated: "1958, April, 20",
-        area: 44000,
-        imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/hamilton-new-zealand-temple/hamilton-new-zealand-temple-37770-main.jpg"
-    },
-    {
-        templeName: "Hong Kong China Temple",
-        location: "Hong Kong, China",
-        dedicated: "1996, May, 26",
-        area: 21500,
-        imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/hong-kong-china-temple/hong-kong-china-temple-37774-main.jpg"
-    }
+  {
+    templeName: "Aba Nigeria",
+    location: "Aba, Nigeria",
+    dedicated: "2005, August, 7",
+    area: 11500,
+    imageUrl:
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/400x250/aba-nigeria-temple-lds-273999-wallpaper.jpg"
+  },
+  {
+    templeName: "Manti Utah",
+    location: "Manti, Utah, United States",
+    dedicated: "1888, May, 21",
+    area: 74792,
+    imageUrl:
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/400x250/manti-temple-768192-wallpaper.jpg"
+  },
+  {
+    templeName: "Payson Utah",
+    location: "Payson, Utah, United States",
+    dedicated: "2015, June, 7",
+    area: 96630,
+    imageUrl:
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/payson-utah/400x225/payson-utah-temple-exterior-1416671-wallpaper.jpg"
+  },
+  {
+    templeName: "Yigo Guam",
+    location: "Yigo, Guam",
+    dedicated: "2020, May, 2",
+    area: 6861,
+    imageUrl:
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/yigo-guam/400x250/yigo_guam_temple_2.jpg"
+  },
+  {
+    templeName: "Washington D.C.",
+    location: "Kensington, Maryland, United States",
+    dedicated: "1974, November, 19",
+    area: 156558,
+    imageUrl:
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/washington-dc/400x250/washington_dc_temple-exterior-2.jpeg"
+  },
+  {
+    templeName: "Lima Perú",
+    location: "Lima, Perú",
+    dedicated: "1986, January, 10",
+    area: 9600,
+    imageUrl:
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/lima-peru/400x250/lima-peru-temple-evening-1075606-wallpaper.jpg"
+  },
+  {
+    templeName: "Mexico City Mexico",
+    location: "Mexico City, Mexico",
+    dedicated: "1983, December, 2",
+    area: 116642,
+    imageUrl:
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg"
+  },
+
+  {
+    templeName: "Durban South Africa",
+    location: "Durban, South Africa",
+    dedicated: "2020, February, 16",
+    area: 19860,
+    imageUrl:
+      "https://churchofjesuschristtemples.org/assets/img/temples/durban-south-africa-temple/durban-south-africa-temple-55461-main.jpg"
+  },
+  {
+    templeName: "Rome Italy",
+    location: "Rome, Italy",
+    dedicated: "2019, March, 10",
+    area: 41010,
+    imageUrl:
+      "https://churchofjesuschristtemples.org/assets/img/temples/rome-italy-temple/rome-italy-temple-40684-main.jpg"
+  },
+  {
+    templeName: "Johannesburg South Africa",
+    location: "Johannesburg, South Africa",
+    dedicated: "1985, August, 24",
+    area: 19184,
+    imageUrl:
+      "https://churchofjesuschristtemples.org/assets/img/temples/johannesburg-south-africa-temple/johannesburg-south-africa-temple-40677-main.jpg"
+  }
 ];
 
 const cards = document.querySelector("#cards");
